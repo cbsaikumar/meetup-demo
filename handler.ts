c44821f9e07e6839bb0877a7851214ee68135ed0
Bhargava Chittapudi <Bhargava_Chittapudi@epam.com>
2021-03-19 10:05:32 +0530

    feature - add s3 and lambda invoke use cases code

--- handler.ts
+++ handler.ts
@@ -1,3 +1,9 @@
+import * as AWS from "aws-sdk";
+import * as fs from "fs";
+
+const S3 = new AWS.S3();
+const Lambda = new AWS.Lambda();
+
 function generatePolicy(principal_id, effect, resource) {
   return {
     principalId: principal_id,
@@ -14,14 +20,75 @@ function generatePolicy(principal_id, effect, resource) {
   };
 }
 export const hello = async (event) => {
+  const params = {
+    FunctionName: "plugins-demo-dev-test",
+    Payload: JSON.stringify({
+      first: "Ravi",
+      second: "Bhargava",
+      third: "Mrinal",
+    }),
+  };
+
+  const response = await Lambda.invoke(params).promise();
+
+  console.log(response);
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        stage: process.env.STAGE,
-        db: process.env.DB_HOST,
-        input: event,
+        message: "Successfully invoked",
+      },
+      null,
+      2
+    ),
+  };
+};
+
+export const updloadFile = async (event) => {
+  try {
+    const body = fs.readFileSync("lambda.png");
+    const params = {
+      Bucket: "demo-meetup",
+      Key: "lambda.png",
+      Body: body,
+    };
+
+    const resp = await S3.putObject(params).promise();
+
+    console.log(resp);
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 400,
+      body: JSON.stringify(
+        {
+          message: "Something went wrong",
+        },
+        null,
+        2
+      ),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "File upload successful",
+      },
+      null,
+      2
+    ),
+  };
+};
+
+export const test = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Hello from anotherLambda",
+        event,
       },
       null,
       2
