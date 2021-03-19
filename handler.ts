6b08e5e4d404d487915053b1704c0716d5371066
Bhargava Chittapudi <Bhargava_Chittapudi@epam.com>
2021-03-19 10:48:13 +0530

    authorizer-demo

--- handler.ts
+++ handler.ts
@@ -1,9 +1,3 @@
-import * as AWS from "aws-sdk";
-import * as fs from "fs";
-
-const S3 = new AWS.S3();
-const Lambda = new AWS.Lambda();
-
 function generatePolicy(principal_id, effect, resource) {
   return {
     principalId: principal_id,
@@ -19,75 +13,13 @@ function generatePolicy(principal_id, effect, resource) {
     },
   };
 }
+
 export const hello = async (event) => {
-  const params = {
-    FunctionName: "plugins-demo-dev-test",
-    Payload: JSON.stringify({
-      first: "Ravi",
-      second: "Bhargava",
-      third: "Mrinal",
-    }),
-  };
-
-  const response = await Lambda.invoke(params).promise();
-
-  console.log(response);
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Successfully invoked",
-      },
-      null,
-      2
-    ),
-  };
-};
-
-export const updloadFile = async (event) => {
-  try {
-    const body = fs.readFileSync("lambda.png");
-    const params = {
-      Bucket: "demo-meetup",
-      Key: "lambda.png",
-      Body: body,
-    };
-
-    const resp = await S3.putObject(params).promise();
-
-    console.log(resp);
-  } catch (err) {
-    console.log(err);
-    return {
-      statusCode: 400,
-      body: JSON.stringify(
-        {
-          message: "Something went wrong",
-        },
-        null,
-        2
-      ),
-    };
-  }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "File upload successful",
-      },
-      null,
-      2
-    ),
-  };
-};
-
-export const test = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Hello from anotherLambda",
+        message: "Successful",
         event,
       },
       null,
